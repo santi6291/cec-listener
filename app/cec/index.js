d46a17f570b56b6c1d3710e53635afb992362094
Santiago J Sosa <ssosa@santiagojsosa.com>
2017-03-04 22:11:33 -0500

    broadcast route change from cec

--- app/cec/index.js
+++ app/cec/index.js
@@ -6,9 +6,11 @@ const cecCtrl = require('./lib/cec-controller');
 // Handle client request for cec command
 wsCtrl.on('onConnectionMessage', (message)=>wsCtrl.server.broadcastUTF(message.utf8Data));
 
-// http server, nedded for websocket
-const server = http.createServer((request, response)=>wsServer.handleHttpResponse(request, response, process.env.WS_PORT));
-server.listen(process.env.WS_PORT, ()=>wsServer.log(`listening ws://localhost:${process.env.WS_PORT}`));
+// broadcast route change
+cecCtrl.on('routeChange', (fromSource, toSource)=>{
+	let msg = JSON.stringify({fromSource, toSource});
+	wsCtrl.server.broadcastUTF(msg)
+});
 
 wsServer.config.httpServer = server;
 const wsInstance = new WebSocketServer(wsServer.config);
