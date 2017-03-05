acbf8152ddbf4a2a4510dddf0370e2cd0b0bf7f0
Santiago J Sosa <ssosa@santiagojsosa.com>
2017-03-04 22:29:24 -0500

    more debug logs

--- app/cec/index.js
+++ app/cec/index.js
@@ -4,16 +4,23 @@ const wsCtrl = require('./lib/ws-controller');
 const cecCtrl = require('./lib/cec-controller');
 
 // Handle client request for cec command
-wsCtrl.on('onConnectionMessage', (message)=>wsCtrl.server.broadcastUTF(message.utf8Data));
+wsCtrl.on('onConnectionMessage', (message)=>{
+	wsCtrl.log('onConnectionMessage')
+	wsCtrl.server.broadcastUTF(message.utf8Data)
+});
 
 // broadcast route change
 cecCtrl.on('routeChange', (fromSource, toSource)=>{
+	cecCtrl.log('routeChange');
 	let msg = JSON.stringify({fromSource, toSource});
 	wsCtrl.server.broadcastUTF(msg)
 });
 
 // broadcast power status
-cecCtrl.on('powerStatus', (status)=>wsCtrl.server.broadcastUTF(status));
+cecCtrl.on('powerStatus', (status)=>{
+	cecCtrl.log('powerStatus');
+	wsCtrl.server.broadcastUTF(status)
+});
 
 
 /* 
