df00eda8fc46296da7388d2d4a1ee70c5fe690f9
Santiago J Sosa <ssosa@santiagojsosa.com>
2017-03-04 23:25:23 -0500

    simplier power check

--- app/cec/lib/cec-controller.js
+++ app/cec/lib/cec-controller.js
@@ -57,15 +57,7 @@ class CECcontroller extends Helper {
 	}
 
 	powerStatusCallback(packet, status){
-		this.log(status)
-		var keys = Object.keys( this.cectypes.PowerStatus );
-
-		for (var i = keys.length - 1; i >= 0; i--) {
-			if (this.cectypes.PowerStatus[keys[i]] == status) {
-				// this.status.on = (keys[i] == 'ON')?  true : false;
-				break;
-			}
-		}
+		this.status.on = (status);
 	}
 }
 module.exports = new CECcontroller();
