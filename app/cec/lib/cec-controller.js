f42fcb62034f425d5959335b29619c0269cb6324
Santiago J Sosa <ssosa@santiagojsosa.com>
2017-03-05 12:46:43 -0500

    cec - test

--- app/cec/lib/cec-controller.js
+++ app/cec/lib/cec-controller.js
@@ -35,7 +35,7 @@ class CECcontroller extends Helper {
 		// -d8 = set log level to 8 (=TRAFFIC) (-d 8)
 		// -br = logical address set to `recording device`
 		// this.cec.start('cec-client', '-m', '-d', '8', '-b', 'r')
-		this.cecSingle.start('cec-client', '-d', '1', '-s')
+		this.cecSingle.start('cec-client', '-s', '-d', '1')
 	}
 	
 	// -------------------------------------------------------------------------- //
