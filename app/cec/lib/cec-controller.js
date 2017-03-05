d680f11d22b945edfe9b62c4dd615eb518b534c1
Santiago J Sosa <ssosa@santiagojsosa.com>
2017-03-04 22:20:32 -0500

    init cec event on constructor

--- app/cec/lib/cec-controller.js
+++ app/cec/lib/cec-controller.js
@@ -17,6 +17,8 @@ class CECcontroller extends Helper {
 		// -------------------------------------------------------------------------- //
 		//- CEC EVENT HANDLING
 		this.cec.once( 'ready', (client)=>this.onReady(client));
+		this.powerStatus();
+		this.routeChange();
 		
 		// -------------------------------------------------------------------------- //
 		// -m  = start in monitor-mode
