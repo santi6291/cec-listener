5eeb0687e7e736a8006b13eed8b4fce4cf03ff09
Santiago J Sosa <ssosa@santiagojsosa.com>
2017-03-05 21:32:08 -0500

    send set active source

--- app/web-app/src/js/app.js
+++ app/web-app/src/js/app.js
@@ -2,11 +2,11 @@ const domCtrl = require('./modules/dom-controller');
 const wsConnection = require('./modules/ws-connection');
 
 domCtrl.onClick('hdmi', (e)=>{
-	console.log(e.target)
+	wsConnection.cecAction('activeSource', `${e.target.dataset.hdmiAddress}.0.0.0`);
 })
 
 domCtrl.onClick('power-button', (e)=>{
-	wsConnection.cecAction('togglePower');	
+	wsConnection.cecAction('togglePower');
 })
 
 wsConnection.onMessage('status', (status)=>{
